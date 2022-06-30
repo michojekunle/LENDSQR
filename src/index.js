92efec908ad79e872f7918d171285f2ca114a761
AMD <michojekunle1@gmail.com>
2022-06-30 02:53:50 -0700

    hey

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const pswStatus = document.querySelector('#pswStatus');
-const passwordInput = document.querySelector('')
+const passwordInput = document.querySelector('.password-container input')
 pswStatus.addEventListener('click', togglePasswordVisibility());
 
 function togglePasswordVisibility() {
-    if()
-}
+    if(passwordInput.type = 'password') {
+        passwordInput.type = 'text';
+    } else  {
+        passwordInput.type = 'password';
+    };
+};
